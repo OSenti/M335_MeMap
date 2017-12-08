7f11da5d7bec4a11f0e17cb31a497ea7c5dc7bb9
OSenti <oliver.senti@hilcona.com>
2017-12-08 12:01:09 +0100

    Added Google Maps
    Yeah!! +1:

--- Memap_M335/src/pages/map/map.ts
+++ Memap_M335/src/pages/map/map.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+
+import {google} from 'google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
+import { CurrentLoc } from '../../interfaces/current-loc';
+
 
 /**
  * Generated class for the MapPage page.
@@ -15,11 +20,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  map: google.maps.Map;
+  currentLoc: CurrentLoc = {lat:0 , lon: 0};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform, public geolocation: Geolocation) {
+      this.map = null;
+
+      geolocation.getCurrentPosition().then(pos => {
+          console.log('lat: ' + pos.coords.latitude + ', lon: ' +
+              pos.coords.longitude);
+      });
+
+      geolocation.getCurrentPosition().then(pos => {
+          console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+          this.currentLoc.lat = pos.coords.latitude;
+          this.currentLoc.lon = pos.coords.longitude;
+          this.currentLoc.timestamp = pos.timestamp;
+      });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+    this.platform.ready().then(() => {
+      this.initializeMap();
+    });
   }
+
+    initializeMap() {
+        let minZoomLevel = 3;
+        this.map = new
+        google.maps.Map(document.getElementById('map_canvas'), {
+            zoom: minZoomLevel,
+            minZoom: 17,
+            maxZoom: 19,
+            center: new google.maps.LatLng(47.413041 ,9.631689),
+            mapTypeControl: false,
+            streetViewControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+    }
 
 }
